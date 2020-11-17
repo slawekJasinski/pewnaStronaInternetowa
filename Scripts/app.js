b106ea1b1a3ba8e9baac4609e6b74900926c6765
Slawek Jasinski <slawomirkaroljasinski@wp.pl>
2020-11-17 22:17:53 +0100

    Sławek

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,5 +1,18 @@
+const messageButton = document.getElementById('message')
+
+let messageButtonHandler = () => {
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let text = document.getElementById('text');
+    console.log(`Wiadomość od ${name.value}/${email.value}: ${text.value}`);
+}
+
 $(document).ready(function(){
     $("message").click(function(event){
         event.preventDefault();
     });
+
+
 });
+
+messageButton.addEventListener('click', messageButtonHandler);
